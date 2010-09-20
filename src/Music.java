e162c948a7abcf3818adebba1b61062efc1d161c
Thomas Dulu <thomas.dulu@gmail.com>
2010-09-20 09:27:27 +0200

    maj...

--- src/Music.java
+++ src/Music.java
@@ -3,7 +3,6 @@ import java.io.*;
 public class Music{
 
 	protected String nom, artiste, album, format;
-	private final String[] formats = {"ogg","mp3","wave","flac"};
 
 	public Music(File zizic){
 
