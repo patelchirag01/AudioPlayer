1b514e1e4ae2ca274d5bb4722a323bfc358592cc
Thomas Dulu <thomas.dulu@gmail.com>
2010-09-20 09:33:53 +0200

    Thomas : je m'occupe de MusicWave.java

---
+++ src/MusicWave.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import javax.sound.sampled.AudioSystem;
+
+
+public class MusicWave{
+
+	protected String nom, artiste, album, format;
+
+	public Music(File zizic){
+
+	}
+
+	protected void play(){
+		
+	}
+
+}
