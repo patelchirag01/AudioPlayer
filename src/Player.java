122e55ee95fed06b8242242bec00529bad82fd88
Thomas Dulu <thomas.dulu@gmail.com>
2010-09-20 10:46:57 +0200

    Player crée, a tester git add src/Player.java !

--- src/Player.java
+++ src/Player.java
@@ -4,38 +4,46 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import java.io.FileInputStream;
+import java.io.File;
 
 public class Player{
 
-protected AudioInputStream audioInput;
+	protected AudioInputStream audioInput;
 
-public Player(AudioInputStream audioInput){
-	this.audioInput = audioInput;
-	play();
-}
-
-public void play(){
-	try{
-		int bytesPerFrame = audioInput.getFormat().getFrameSize();
-		int numBytes = 1024 * bytesPerFrame;
-		byte[] tableau = new byte[numBytes];
-
-
-		AudioFormat audioFormat = AudioInput.getFormat();
-		DataLine.Info Info = new DataLine.Info(SourceDataLine.class,audioFormat);
-
-		SourceDataLine line=(SourceDataLine)AudioSystem.getLine(Info);
-		line.open(audioFormat);
-		line.start();
-
-		int nb;
-		while ( (nb = AudioInput.read(tableau,0,numBytes )) != -1 ){
-			line.write(tableau,0,nb);
-		}
-	}catch (Exception e){
-	e.printStackTrace();
+	public Player(AudioInputStream audioInput){
+		this.audioInput = audioInput;
+		play();
 	}
-} 
+
+	public void play(){
+		try{
+			int bytesPerFrame = audioInput.getFormat().getFrameSize();
+			int numBytes = 1024 * bytesPerFrame;
+			byte[] tableau = new byte[numBytes];
 
 
+			AudioFormat audioFormat = audioInput.getFormat();
+			DataLine.Info Info = new DataLine.Info(SourceDataLine.class,audioFormat);
+
+			SourceDataLine line=(SourceDataLine)AudioSystem.getLine(Info);
+			line.open(audioFormat);
+			line.start();
+
+			int nb;
+			while ( (nb = audioInput.read(tableau,0,numBytes )) != -1 ){
+				line.write(tableau,0,nb);
+			}
+		}catch (Exception e){
+		e.printStackTrace();
+		}
+	} 
+	public static void main(String args[]){
+		MusicWave music;
+		try {
+			music = new MusicWave(new File("../Music/1-welcome.wav"));
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		new Player(music.renvoiAudioInputStream());
+	}
 }
